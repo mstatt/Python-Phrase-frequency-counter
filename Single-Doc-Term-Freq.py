8a6e761e5d914bdc9ae2cbeac872d0b7ad634ba2
Michael Stattelman <mike@supercess.com>
2017-08-04 10:20:54 -0400

    Update
    More code cleanup

--- Single-Doc-Term-Freq.py
+++ Single-Doc-Term-Freq.py
@@ -4,29 +4,22 @@ Created on Sat Jul 29 10:11:14 2017
 
 @author: MStattelman
 """
-
 import re
 import string
 import collections
 from collections import Counter
-
 
 frequency = {}
 document_text = open('YOURDIRECTORY/YOURFILE', 'r')
 text_string = document_text.read().lower()
 match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
 
-
- 
 for word in match_pattern:
     count = frequency.get(word,0)
     frequency[word] = count + 1
-     
+
 frequency_list = frequency.keys()
 od = collections.OrderedDict(sorted(frequency.items(),key=lambda t: t[1], reverse=True))
-
-
-
 
 import pandas as pd
 df = pd.DataFrame.from_dict(od, orient='index').reset_index()
@@ -36,8 +29,3 @@ df.to_html(open('SingleWord.html', 'w'))
 #Convert to pdf.
 #import pdfkit
 #pdfkit.from_url('SingleWord.html', 'SingleWord.pdf')
-
-
-
-
-
